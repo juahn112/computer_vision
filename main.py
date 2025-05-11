a722f6d07454298e5413f6ae1e094ee6f2003968
신주안 <juahn112@gmail.com>
2025-05-12 01:34:57 +0900

    꽃 이미지 덮기 기능 추가가

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ haar = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_de
 
 net = cv2.dnn.readNetFromCaffe('deploy.prototxt','res10_300x300_ssd_iter_140000.caffemodel')
 
+flower = cv2.imread('flower.png', cv2.IMREAD_UNCHANGED)
+
 if haar.empty():
     print("face cascade error")
 else:
@@ -34,7 +36,16 @@ while True:
 
         confidence = detections[0, 0, 0, 2]  
         if confidence > 0.6:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            flower_resized = cv2.resize(flower, (w,h))
+
+            flower_rgb = flower_resized[:, :, :3]
+            alpha_mask = flower_resized[:,:,3] / 255.0
+
+            for c in range(3):
+                frame[y:y+h, x:x+w, c] = (
+                    alpha_mask * flower_rgb[:,:,c]+
+                    (1-alpha_mask) * frame[y:y+h, x:x+w, c]
+                )
     
     cv2.imshow('Webcam View', frame)
 
